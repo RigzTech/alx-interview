73ea829ebe16f9053609caa7c370afbbeb0c6d3d
RigzTech <fredrickarara@gmail.com>
2024-07-25 13:35:48 -0700

    utf8-validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,44 +1,28 @@
-
-dule for validUtf8 method"""
+ding_set_bits(num):
+    """returns the number of leading set bits (1)"""
+    set_bits = 0
+    helper = 1 << 7
+    while helper & num:
+        set_bits += 1
+        helper = helper >> 1
+    return set_bits
 
 
 def validUTF8(data):
-    """Determines if given data represents valid UTF-8 encoding
-    Args:
-        data: list of integers
-    Returns:
-        True if valid UTF-8 encoding, otherwise False
-    """
-    # Number of bytes in the current UTF-8 character
-    n_bytes = 0
-
-    # Mask to check if the most significant bit is set or not
-    mask1 = 1 << 7
-
-    # Mask to check if the second most significant bit is set or not
-    mask2 = 1 << 6
-    for num in data:
-
-        # Get the number of set most significant bits in the byte if
-        # this is the starting byte of an UTF-8 character.
-        mask = 1 << 7
-        if n_bytes == 0:
-            while mask & num:
-                n_bytes += 1
-                mask = mask >> 1
-
-            # 1 byte characters
-            if n_bytes == 0:
+    """determines if a given data set represents a valid UTF-8 encoding"""
+    bits_count = 0
+    for i in range(len(data)):
+        if bits_count == 0:
+            bits_count = get_leading_set_bits(data[i])
+            '''1-byte (format: 0xxxxxxx)'''
+            if bits_count == 0:
                 continue
-
-            # Invalid scenarios according to the rules of the problem.
-            if n_bytes == 1 or n_bytes > 4:
+            '''a character in UTF-8 can be 1 to 4 bytes long'''
+            if bits_count == 1 or bits_count > 4:
                 return False
         else:
-            # If this byte is a part of an existing UTF-8 character, then we
-            # simply have to look at the two most significant bits and we make
-            # use of the masks we defined before.
-            if not (num & mask1 and not (num & mask2)):
+            '''checks if current byte has format 10xxxxxx'''
+            if not (data[i] & (1 << 7) and not (data[i] & (1 << 6))):
                 return False
-        n_bytes -= 1
-    return n_bytes == 0
+        bits_count -= 1
+    return bits_count == 0
