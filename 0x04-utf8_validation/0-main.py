9638fe0664943be207535560b137668c502bd722
RigzTech <fredrickarara@gmail.com>
2024-07-26 02:41:49 -0700

    utf8 validation

--- 0x04-utf8_validation/0-main.py
+++ 0x04-utf8_validation/0-main.py
@@ -1,4 +1,6 @@
-in file for testing
+#!/usr/bin/python3
+"""
+Main file for testing
 """
 
 validUTF8 = __import__('0-validate_utf8').validUTF8
@@ -10,13 +12,4 @@ data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
 print(validUTF8(data))
 
 data = [229, 65, 127, 256]
-print(validUTF8(data))
-
-data = [197, 130, 1]
-print(validUTF8(data))
-
-data = [235, 140, 4]
-print(validUTF8(data))
-
-data = [250, 145, 145, 145, 145]
 print(validUTF8(data))
